a6d2ee50f2af4f8f280e60bf4435b4ab590a6df0
Simon Blomsterlund <simon@simonblund.com>
2019-07-25 10:11:58 +0300

    part 1 finished

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,12 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = ({onClick, text}) => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+        <button onClick={onClick}>{text}</button>
+    )
+}
+
+const Anecdote = ({anecdote, votes}) => {
+    return (
+        <p>
+            {anecdote}<br></br>
+            <strong>{votes}</strong>
+        </p>
+        
+
+    )
+}
+
+
+const App = ({anecdotes, points, rand}) => {
+    const [selected, setSelected] = useState(rand)
+
+    const handleNewAnecdote = () => {
+        let min = 0
+        let max = anecdotes.length
+        setSelected(Math.floor(Math.random() * (+max - +min)) + +min)
+    }
+    const handleNewVote = () => {
+        points[selected] = points[selected]+1
+        refresh()
+    }
+
+    
+    return (
+      <div>
+        <h1>Anecdote of the day</h1>
+        <Anecdote anecdote = {anecdotes[selected]} votes ={points[selected]}/>
+        <Button onClick = {handleNewAnecdote} text="Random"/>
+        <Button onClick = {handleNewVote} text="Vote"/>
+        <hr></hr>
+        <h1>Anecdote with most votes</h1>
+        <Anecdote anecdote={anecdotes[points.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)]} votes = {Math.max.apply(null,points)}/>
+      </div>
+    )
+  }
+  
+  const points = Array.apply(null, new Array(6)).map(Number.prototype.valueOf,0);
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+
+  const randomAnecdote = () => {
+    let min = 0
+    let max = anecdotes.length
+    return Math.floor(Math.random() * (+max - +min)) + +min
+    }
+  
+  ReactDOM.render(
+    <App anecdotes={anecdotes} points = {points} rand= {randomAnecdote}/>,
+    document.getElementById('root')
+  )
+
+  const refresh = () => {
+    ReactDOM.render(<App  anecdotes={anecdotes} points = {points}/>, 
+    document.getElementById('root'))
+  }
+
