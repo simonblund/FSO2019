bbf2d0b23b95768518752aaba64e1c745d91aac5
Simon Blomsterlund <simon@simonblund.com>
2019-08-02 11:57:58 +0300

    Finished

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,30 +1,32 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './components/Form'
 import List from './components/List'
 import Search from './components/Search'
+import Notification from './components/Notification'
 import './App.css';
 import contactService from './services/contactService'
 
 const App = () => {
-  const [ persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([])
   const [filterPersons, setFilterPersons] = useState(persons);
-  const [ newName, setNewName ] = useState('')
-  const [ newPhone, setNewPhone ] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+  const [errorMessage, setErrorMessage] = useState({})
 
-  
+
   const personHook = () => {
     contactService
-    .getAll()
-    .then(response => {
-      setPersons(response)
-      setFilterPersons(response)
-    })
-    
+      .getAll()
+      .then(response => {
+        setPersons(response)
+        setFilterPersons(response)
+      })
+
   }
   useEffect(personHook, [])
-  
 
-  const handleNameChange = (event) =>{
+
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
   const handlePhoneChange = (event) => {
@@ -32,54 +34,116 @@ const App = () => {
   }
 
   const handleFilter = (event) => {
-    setFilterPersons(persons.filter(person => person.name.toLowerCase().indexOf(event.target.value.toLowerCase())!==-1))
+    setFilterPersons(persons.filter(person => person.name.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1))
   }
 
-  const addPerson = (event)=>{
+  // Add or edit persn
+  const addPerson = (event) => {
+    // prevent the form's default action
     event.preventDefault()
-    const personObj = {
+
+    // This is how the person object looks.
+    let personObj = {
       name: newName,
-      number: newPhone,
+      number: newPhone
     }
-    if(persons.find(i => i.name === personObj.name)){
-      const changeConf = window.confirm(newName+' is already added to phonebook, would you like to change the number?')
-      if(changeConf) {
-        const changee = persons.find(i => i.name === personObj.name )
-        contactService.update(changee.id, personObj)
-      }
-    } else{
-        contactService.create(personObj)
+    // If person with name exists, ask if you want to update that person instead.
+    if (persons.find(i => i.name === personObj.name)) {
+      const changeConf = window.confirm(newName + ' is already added to phonebook, would you like to change the number?')
+      // If you want to update the person
+      if (changeConf) {
+        const changee = persons.find(i => i.name === personObj.name)
+
+        // Update the person with the found id
         contactService
-          .getAll()
-          .then(response => {
-            setPersons(response)
-            setFilterPersons(response)
+          .update(changee.id, personObj)
+          .catch(error => {
+            setFilterPersons(filterPersons.filter(fp => fp.id !== changee.id))
+            setErrorMessage(
+              {
+                type: 'error',
+                message: 'Update of ' + personObj.name + error+ ' failed'
+              }
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
           })
         
+        changee.number = personObj.number
+
+        // If update succeeded notify
+        setErrorMessage(
+          {
+            type: 'success',
+            message: 'Person ' + personObj.name + ' was updated'
+          }
+
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      }
+    } else {
+      // If no person with name exists, create a new person
+      contactService
+        .create(personObj)
+        .then( response => {
+          personObj = response
+          setPersons(persons.concat(personObj))
+          setFilterPersons(filterPersons.concat(personObj))
+        })
+        .catch(error => {
+          // Catch errors on create
+          console.log('On create', error)
+          setErrorMessage(
+            {
+              type: 'error',
+              message: 'Creation of ' + personObj.name + ' failed'
+            }
+
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+        })
+
+      // On success notify
+      setErrorMessage(
+        {
+          type: 'success',
+          message: 'Person ' + personObj.name + ' was created'
+        }
+
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     }
+    
     setNewName('')
     setNewPhone('')
   }
 
   let list = () => {
-    if(filterPersons !== null){
+    if (filterPersons !== null) {
       return <List persons={filterPersons} />
     } else {
       return <p>No persons in list</p>
     }
   }
-  
-  const handleChange = {name:handleNameChange, number:handlePhoneChange}
-  const newVariables = {name: newName, number:newPhone,}
+
+  const handleChange = { name: handleNameChange, number: handlePhoneChange }
+  const newVariables = { name: newName, number: newPhone, }
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <div>debug: {newName} {newPhone}</div>
+      <Notification message={errorMessage} />
       <h2>Search</h2>
-      <Search onChange={handleFilter}/>
+      <Search onChange={handleFilter} />
       <h2>Add new</h2>
-      <Form handleChange = {handleChange} action = {addPerson} newVariables ={newVariables}/>
+      <Form handleChange={handleChange} action={addPerson} newVariables={newVariables} />
       <h2>List</h2>
       {list()}
     </div>
